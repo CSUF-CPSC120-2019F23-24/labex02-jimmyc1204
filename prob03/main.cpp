73e54c1f438e7857a84b242ee9251c5716b0b5b7
JimmyC1204 <jimmyc1204@cs104u-2c.acad.fullerton.edu>
2019-08-31 14:56:14 -0700

    created a calculator for a bill

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,29 @@
-// Name: 
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double cost, Tax, tip, totalbill;
+  std::cout << "Welcome to the Restaurant Bill Calculator!" << '\n'; std::cout << "What is the total meal cost?";
+  std::cin >> cost;
+
+//calulations for tax
+  Tax= cost * .0775;
+  std::cout << "Tax   " << Tax <<'\n';
+
+//calculations for tip
+  tip= cost * .2;
+  std::cout << "Tip   " << tip <<'\n';
+
+//calculations for total Bill
+  totalbill= cost + Tax + tip;
+  std::cout << "Total Bill   " << totalbill << '\n';
+
+
+
+
+  return 0;
+
+}
